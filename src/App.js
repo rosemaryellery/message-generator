db0b4f053db1ca628a6e1aabc0f0e8b11736dd6f
rosemaryellery <98529835+rosemaryellery@users.noreply.github.com>
2022-08-27 22:02:48 +0100

    'App  now produces non-randomised message.'

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
 import './App.css';
-import { messages } from './Messages';
+import {useRef} from 'react';
 
 function App() {
+  const object = useRef(null);
+  const place = useRef(null);
+  const adj = useRef(null);
   return (
     <div className="App">
-      <h1>Message Generator</h1>
-        <TakeInputs />
+      <h1>Message Generator</h1><br/>
+      <form>
+        <label for='object-form'>Type the first object that comes to mind: </label>
+        <input type='text' id='object-form' ref={object}></input><br/><br/>
+        <label for='place-form'>Type the first place that comes to mind: </label>
+        <input type='text' id='place-form' ref={place}></input><br/><br/>
+        <label for='adj-form'>Type the first adjective that comes to mind: </label>
+        <input type='text' id='adj-form' ref={adj}></input><br/><br/>
+      </form>
+      <button onClick={GenerateMessage}>Generate Message</button><br/>
+      <p id='fortune'></p>
     </div>
   );
-}
-
-function GetMessage() {
-  
-}
-
-function TakeInputs() {
-  return (
-    <div>
-      <form>
-        <label for='text-form'>Type the first object that comes to mind: </label>
-        <input type='text' id='text-form'></input><br/><br/>
-        <label for='text-form'>Type the first object that comes to mind: </label>
-        <input type='text' id='text-form'></input><br/><br/>
-        <label for='text-form'>Type the first object that comes to mind: </label>
-        <input type='text' id='text-form'></input><br/><br/>
-      </form>
-    </div>
-  )
+  function GenerateMessage() {
+    document.getElementById('fortune').innerHTML = 'You may find a ' + object.current.value + ' in ' + place.current.value + ' if you are ' + adj.current.value + ' enough.';
+  }
 }
 
 export default App;
